29d2800dd2a360c08ce9101e2783737470a76655
Luiz Ferreira <84511085+luizferreira7@users.noreply.github.com>
2023-12-29 10:39:54 -0300

    Cria DTO para resposta da API
    #4

---
+++ StockQuoteAlert/StockQuoteAlert/Model/StockPriceDTO.cs
@@ -0,0 +1,19 @@
+namespace StockQuoteAlert.Model;
+
+public class StockPriceDTO
+{
+    public string Stock { get; set; }
+    public double Price { get; set; }
+    public string Status { get; set; }
+ 
+    public StockPriceDTO()
+    {
+    }
+
+    public StockPriceDTO(string stock, double price, string status)
+    {
+        Stock = stock;
+        Price = price;
+        Status = status;
+    }
+}
