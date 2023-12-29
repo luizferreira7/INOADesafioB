7e9df0a29d27cd72e259970dc1f06b6c521eca85
Luiz Ferreira <84511085+luizferreira7@users.noreply.github.com>
2023-12-28 22:19:32 -0300

    Adicionando endpoint para obter preço de um ativo
    #4

--- MockAPI/MockAPI/Program.cs
+++ MockAPI/MockAPI/Program.cs
@@ -1,3 +1,5 @@
+using MockAPI.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +18,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/finance/stock_price", (string stock) =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
+        StockDTO stockDTO = new();
+
+        return stock;
     })
-    .WithName("GetWeatherForecast")
+    .WithName("GetStockPrice")
     .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
