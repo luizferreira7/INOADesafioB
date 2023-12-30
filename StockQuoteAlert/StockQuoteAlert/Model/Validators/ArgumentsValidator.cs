e492acb4364dc7e801debc679987f6710e0478c4
Luiz Ferreira <84511085+luizferreira7@users.noreply.github.com>
2023-12-30 16:29:25 -0300

    Adiciona validação de ativo existente
    #3

--- StockQuoteAlert/StockQuoteAlert/Model/Validators/ArgumentsValidator.cs
+++ StockQuoteAlert/StockQuoteAlert/Model/Validators/ArgumentsValidator.cs
@@ -1,27 +1,58 @@
+using System.Net;
 using StockQuoteAlert.Constants;
 using StockQuoteAlert.Exceptions;
+using StockQuoteAlert.Business;
+using StockQuoteAlert.Utility;
 
 namespace StockQuoteAlert.Model.Validators;
 
 public class ArgumentsValidator : IValidator
 {
-    private Arguments Arguments { get; set; }
+    protected Arguments _arguments;
+    protected RequestHandler _requestHandler;
+
+    protected string _apiUrl;
     
-    public ArgumentsValidator(Arguments arguments)
+    protected string _apiPath;
+    
+    public ArgumentsValidator(Arguments arguments, string apiUrl, string apiPath)
     {
-        Arguments = arguments;
+        _arguments = arguments;
+        _requestHandler = new RequestHandler();
+        _apiUrl = apiUrl;
+        _apiPath = apiPath;
     }
 
     public void Validate()
     {
         BuyPriceBiggerThanSellPrice();
+        StockExists();
     }
 
     private void BuyPriceBiggerThanSellPrice()
     {
-        if (Arguments.BuyPrice > Arguments.SellPrice)
+        if (_arguments.BuyPrice > _arguments.SellPrice)
         {
             throw new ValidationException(ValidationErrorCode.BUYPRICE_BIGGERTHAN_SELLPRICE);
+        }
+    }
+
+    private async void StockExists()
+    {
+        var paramMap = new Dictionary<string, string>();
+
+        paramMap.Add(Params.STOCK_PARAM, _arguments.Stock);
+        
+        try
+        {
+            await _requestHandler.MakeRequest(_apiUrl, _apiPath, paramMap);
+        }
+        catch (RequestErrorException e)
+        {
+            if (e.Status == (int) HttpStatusCode.NotFound)
+            {
+                throw new ValidationException(ValidationErrorCode.STOCK_NOT_FOUND);
+            }
         }
     }
 }
