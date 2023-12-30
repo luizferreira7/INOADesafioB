111252fcee6fe34e5149af5ef6cb5c68e8919b5b
Luiz Ferreira <84511085+luizferreira7@users.noreply.github.com>
2023-12-30 10:49:12 -0300

    Adiciona Handler para UnhandledException

--- StockQuoteAlert/StockQuoteAlert/Program.cs
+++ StockQuoteAlert/StockQuoteAlert/Program.cs
@@ -2,10 +2,20 @@
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        var runner = new Runner();
+        var currentDomain = AppDomain.CurrentDomain;
+        currentDomain.UnhandledException += (Handler);
         
+        var runner = new Runner();
         runner.Start(args);
+    }
+    
+    private static void Handler(object sender, UnhandledExceptionEventArgs args)
+    {
+        Exception e = (Exception) args.ExceptionObject;
+        
+        Console.WriteLine(e.Message);
+        Console.WriteLine(e.StackTrace);
     }
 }
