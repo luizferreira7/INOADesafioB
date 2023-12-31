2285109f03be3345021ef4f533e51556cc35f3a1
Luiz Ferreira <84511085+luizferreira7@users.noreply.github.com>
2023-12-30 21:52:08 -0300

    Retorna 404 caso não encontre o ativo

--- StockPriceAPI/app.py
+++ StockPriceAPI/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from model.entities import Stock
 
 import yfinance as yf
@@ -8,14 +8,16 @@ app = Flask(__name__)
 
 @app.route('/finance/stock_price', methods=['GET'], )
 def get_stock():
-
     args = request.args
 
     stock = args['stock']
 
     stockData = yf.Ticker(stock + ".SA")
 
-    price = stockData.info.get('regularMarketPrice', stockData.info.get('currentPrice'))
+    try:
+        price = stockData.info.get('regularMarketPrice', stockData.info.get('currentPrice'))
+    except:
+        abort(404)
 
     stockPrice = Stock(stockData.info.get('symbol'), price)
 
@@ -25,4 +27,4 @@ def get_stock():
 port_number = 5132
 
 if __name__ == '__main__':
-    app.run(port=port_number,  host="localhost")
+    app.run(port=port_number, host="localhost")
